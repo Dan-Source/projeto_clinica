943c8ff1edc57976dab1d95a302c27815b7d52a3
Dan-source <dan.dluis.dl@gmail.com>
2022-01-05 19:14:05 -0300

    Adiciona o modelo e o admin de agenda

--- medicos/admin.py
+++ medicos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from medicos.models import Especialidade, Medico
+from medicos.models import Especialidade, Medico, Agenda
 
 class EspecialidadeAdmin(admin.ModelAdmin):
     list_display = ['nome']
@@ -10,5 +10,11 @@ class MedicoAdmin(admin.ModelAdmin):
         'nome', 'crm', 'telefone',
     ]
     
+class AgendaAdmin(admin.ModelAdmin):
+    list_display = [
+        'dia', 'medico', 'horario'
+    ]
+    
 admin.site.register(Especialidade, EspecialidadeAdmin)
 admin.site.register(Medico, MedicoAdmin)
+admin.site.register(Agenda, AgendaAdmin)
--- medicos/models.py
+++ medicos/models.py
@@ -1,5 +1,7 @@
+from datetime import date
 from django.db import models
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from django.db.models.fields.related import ForeignKey
 
 class Especialidade(models.Model):
@@ -18,3 +20,21 @@ class Medico(models.Model):
                                 validators=[phone_regex],
                                 max_length=17, null=True, blank=True)
     especialidade = ForeignKey(Especialidade, on_delete=models.CASCADE, related_name='medicos')
+
+def validar_dia(value):
+    today = date.today()
+    if value < today:
+        raise ValidationError('Não é possivel escolher um data atrasada.')
+
+class Agenda(models.Model):
+    medico = ForeignKey(Medico, on_delete=models.CASCADE, related_name='agenda')
+    dia = models.DateField(help_text="Insira uma data para agenda", validators=[validar_dia])
+    
+    HORARIOS = (
+        ("1", "07:00 ás 08:00"),
+        ("2", "08:00 ás 09:00"),
+        ("3", "09:00 ás 10:00"),
+        ("4", "10:00 ás 11:00"),
+        ("5", "11:00 ás 12:00"),
+    )
+    horario = models.CharField(max_length=10, choices=HORARIOS)
