850cac67e8ef72b333b648698f9a5a6a5183f872
Dan-source <dan.dluis.dl@gmail.com>
2022-01-08 12:52:35 -0300

    Adiciona restrição as cadastro de medicos e especialidades pelo url

--- medicos/views.py
+++ medicos/views.py
@@ -1,23 +1,54 @@
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Medico, Agenda, Especialidade
 
-class MedicoCreateView(LoginRequiredMixin ,CreateView):
+
+class TestMixinIsAdmin(UserPassesTestMixin):
+    def test_func(self):
+        is_admin_or_is_staff = self.request.user.is_superuser or \
+            self.request.user.is_staff
+        return bool(is_admin_or_is_staff)
+
+    def handle_no_permission(self):
+        messages.error(
+            self.request, "Você não tem permissões!"
+        )
+        return redirect("accounts:index")
+
+class MedicoCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
 
     model = Medico
     login_url = 'accounts:login'
     template_name = 'medicos/cadastro.html'
-    fields = ['nome', 'crm', 'telefone', 'especialidade']
-    success_url = reverse_lazy('index')
+    fields = ['nome', 'crm', 'email', 'telefone', 'especialidade']
+    success_url = reverse_lazy('medicos:medicos_lista')
     
-class EspecialidadeCreateView(LoginRequiredMixin ,CreateView):
+class MedicoListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
+    
+    login_url = 'accounts:login'
+    template_name = 'medicos/medicos_list.html'
+
+    def get_queryset(self):
+        return Medico.objects.all().order_by('-pk')
+    
+class EspecialidadeCreateView(LoginRequiredMixin, TestMixinIsAdmin, CreateView):
 
     model = Especialidade
     login_url = 'accounts:login'
     template_name = 'medicos/cadastro.html'
     fields = ['nome',]
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('medicos:agenda_lista')
+    
+class EspecialidadeListView(LoginRequiredMixin, TestMixinIsAdmin, ListView):
+    
+    login_url = 'accounts:login'
+    template_name = 'medicos/especialidade_list.html'
+
+    def get_queryset(self):
+        return Especialidade.objects.all().order_by('-pk')
 
 
 class AgendaCreateView(LoginRequiredMixin ,CreateView):
@@ -42,6 +73,8 @@ class AgendaListView(LoginRequiredMixin, ListView):
         return Agenda.objects.filter(user=self.request.user).order_by('-pk')
     
 medico_cadastro = MedicoCreateView.as_view()
+medico_lista = MedicoListView.as_view()
 especialidade_cadastro = EspecialidadeCreateView.as_view()
+especialidade_lista = EspecialidadeListView.as_view()
 agenda_cadastro = AgendaCreateView.as_view()
 agenda_lista = AgendaListView.as_view()
